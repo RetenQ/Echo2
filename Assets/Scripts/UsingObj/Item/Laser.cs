1d9be71b6cdee0407ecc326a8792d7d615fc6e3f
Hongyi.Lan <18965093000@163.com>
2024-04-21 13:50:42 +0800

    存储激光

--- Assets/Scripts/UsingObj/Item/Laser.cs
+++ Assets/Scripts/UsingObj/Item/Laser.cs
@@ -4,14 +4,27 @@ public class Laser : MonoBehaviour
 {
     public GameObject destination; // 目标物体
     private LineRenderer lineRenderer; // LineRenderer组件
+    public string targetTag;
+    public float damage;
+    public BaseObj LaserUser; 
+
+    [Header("准备状态属性")]
+    public Color IniColor;
+
+    public float IniWidth; 
+
+        
+    [Header("激活状态属性")]
+    public Color ActColor;
+    public float ActWidth;
+
+
 
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.startColor = Color.red;
-        lineRenderer.endColor = Color.red;
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
+
+
     }
 
     private void Update()
@@ -27,9 +40,23 @@ public class Laser : MonoBehaviour
         }
     }
 
+    public void Reset(GameObject _destination , string _targetTag , BaseObj _user)
+    {
+        this.destination = _destination;
+        this.targetTag = _targetTag;
+        this.LaserUser= _user;
+    }
+
     // 在挂载函数的物体和一个名为destination的gameObject之间绘制一条红色的线
     public void SetLaser()
     {
+        lineRenderer.material.color = IniColor; // 设置线段颜色
+
+        /*        lineRenderer.startColor = InistartColor;
+                lineRenderer.endColor = IniendColor;*/
+        lineRenderer.startWidth = IniWidth;
+        lineRenderer.endWidth = IniWidth;
+
         if (destination != null)
         {
             lineRenderer.positionCount = 2;
@@ -43,14 +70,21 @@ public class Laser : MonoBehaviour
     {
         if (destination != null)
         {
-            lineRenderer.startColor = Color.yellow;
-            lineRenderer.endColor = Color.yellow;
+            lineRenderer.material.color = ActColor; // 设置线段颜色
+            /*            lineRenderer.startColor = ActstartColor;
+                        lineRenderer.endColor = ActendColor;*/
+            lineRenderer.startWidth = ActWidth;
+            lineRenderer.endWidth = ActWidth;
 
             RaycastHit2D[] hits = Physics2D.LinecastAll(transform.position, destination.transform.position);
 
             foreach (RaycastHit2D hit in hits)
             {
-                Debug.Log("Hit object name: " + hit.transform.name);
+                // Debug.Log("Hit object name: " + hit.transform.name);
+                if (hit.transform.gameObject.CompareTag(targetTag))
+                {
+                    hit.transform.gameObject.GetComponent<BaseObj>().Hurt(damage , LaserUser);
+                }
             }
 
             //SetLaser(); // 绘制橙色的线
